74e10e9eb212f0615605cd6300a7465f9d8d8b55
glebYu <gleb.yurikk@gmail.com>
2023-10-05 20:04:17 +0300

    v0.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: { port: 3345, host: false }
 });
